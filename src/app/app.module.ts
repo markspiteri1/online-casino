e8655ea2d89495897ec0c89bba367e14b43dfcf1
Mark Spiteri <markspiteri1@gmail.com>
2021-03-10 21:50:41 +0100

    feat: add simple menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { AppPagesModule } from "./pages/pages.module";
 
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 @NgModule({
 	declarations: [AppComponent],
 	imports: [
@@ -15,6 +23,12 @@ import { AppPagesModule } from "./pages/pages.module";
 		RouterModule,
 		BrowserModule,
 		AppPagesModule,
+		MatToolbarModule,
+		MatSidenavModule,
+		MatListModule,
+		MatButtonModule,
+		MatIconModule,
+		BrowserAnimationsModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
