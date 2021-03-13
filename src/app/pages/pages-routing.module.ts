2fc486e27925d319f412a2fa027750ba0c8e2aea
Mark Spiteri <markspiteri1@gmail.com>
2021-03-13 09:24:52 +0100

    feat: create component and route for games

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,16 +3,20 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
 
+import { GamesComponent } from "./../components/games/games.component";
+
 const ROUTES: Routes = [
-	{ path: "", component: HomeComponent },
+	{ path: "", redirectTo: "home", pathMatch: "full" },
+	{ path: "games", component: GamesComponent },
+	{ path: "home", component: HomeComponent },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(ROUTES, {
-		// enableTracing: true
-	})],
+	imports: [
+		RouterModule.forRoot(ROUTES, {
+			// enableTracing: true
+		}),
+	],
 	exports: [RouterModule],
 })
-export class AppPagesRoutingModule {
-
-}
+export class AppPagesRoutingModule {}
